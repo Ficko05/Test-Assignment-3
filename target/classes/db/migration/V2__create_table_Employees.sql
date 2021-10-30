fb2950f75db7026165e4b210ca3e14ea34883a25
Ficko05 <filip-f@hotmail.com>
2021-10-30 10:58:06 +0200

    fix

---
+++ target/classes/db/migration/V2__create_table_Employees.sql
@@ -0,0 +1,7 @@
+create table Employee (
+       ID int not null auto_increment,
+       firstname varchar(255) not null,
+       lastname varchar(255) not null,
+       birthdate date,
+       PRIMARY KEY (ID)
+ )
